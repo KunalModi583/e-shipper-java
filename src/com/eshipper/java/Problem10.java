04c0731bd8ece6b88a902c74f7ea4f5db6ecb173
Kunal Modi <kuna.modi@ext.zestmoney.in>
2022-06-13 11:01:12 +0530

    Problem 10 updated

--- src/com/eshipper/java/Problem10.java
+++ src/com/eshipper/java/Problem10.java
@@ -8,8 +8,18 @@ import java.util.Arrays;
 public class Problem10 {
 
 	public static void main(String[] args) {
-		int[] list1 = { 1, 45, 76, 99, 12, 48, 12, 55, 100, 111 };
-		Arrays.sort(list1);
-		System.out.println(list1[list1.length - 2]);
+		int[] list1 = { 1, 45, 76, 99, 12, 48, 12, 55, 100 };
+		int max = Math.max(list1[0], list1[1]);
+		int secondMax = Math.min(list1[0], list1[1]);
+		int len = list1.length;
+		for (int i = 2; i < len; i++) {
+			if (list1[i] > max) {
+				secondMax = max;
+				max = list1[i];
+			} else if (list1[i] > secondMax && max != list1[i]) {
+				secondMax = list1[i];
+			}
+		}
+		System.out.println("Second highest number is: " + secondMax);
 	}
 }
